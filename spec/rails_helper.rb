beba57a619b9f14389d83bf47f5adfc06862a6cc
Lucas Medeiros <lucastoc@gmail.com>
2018-01-15 22:06:53 -0300

    add more conf

---
+++ spec/rails_helper.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_group 'Repositories', 'app/repositories'
+    add_group 'Services', 'app/services'
+  end
+end
+
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'api_matchers'
+require 'database_cleaner'
+require 'factory_bot_rails'
+require 'rails-controller-testing'
+require 'rspec/rails'
+require 'shoulda-matchers'
+require 'timecop'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+APIMatchers.setup do |config|
+  config.header_method = :headers
+  config.header_content_type_key = 'Content-Type'
+  config.response_body_method = :body
+  config.http_status_method = :status
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  %i[controller view request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
+  config.include APIMatchers::RSpecMatchers
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
