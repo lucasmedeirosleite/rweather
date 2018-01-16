67c6bdcaa22786a9b6bc520e320a3b3f5972ed95
Lucas Medeiros <lucastoc@gmail.com>
2018-01-16 14:17:38 -0300

    add specs for the client

--- lib/open_weather/client.rb
+++ lib/open_weather/client.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
+require 'httparty'
+
 module OpenWeather
   class Client
-    Response = Struct.new(:status, :response, :success?)
+    Response = Struct.new(:status, :content, :success?)
 
-    def initialize(config: OpenWeather)
+    def initialize(config: OpenWeather, http_client: HTTParty)
       @config = config
+      @http_client = http_client
     end
 
     def get(uri, params: {})
-      Response.new(nil, nil, false)
+      url = build_url(uri)
+      params = build_query(params)
+
+      response = HTTParty.get(url, params)
+      code = response.code
+      Response.new(response.code, response.parsed_response, code >= 200 && code < 400 )
     end
 
     private
 
-    attr_reader :config
+    attr_reader :config, :http_client
+    delegate :base_url, :api_key, to: :config
+
+    def build_url(uri)
+      "#{base_url}/#{uri}"
+    end
+
+    def build_query(params = {})
+      params = { appid: api_key }.merge(params)
+      { query: params }
+    end
   end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -13,28 +13,60 @@ describe OpenWeather::Client do
   describe '#get' do
     subject(:get_data) { client.get(url, params: params) }
 
+    let(:url) { 'weather' }
     let(:params) do
       { lat: -3.72, lon: -38.52 }
     end
 
     context 'when base_url invalid' do
-      let(:base_url) { 'http://ivalid-weather.com' }
-      let(:url) { "#{base_url}/weather" }
+      let(:base_url) { 'http://www.google.com' }
 
       it 'does not succeeds' do
-        VCR.use_cassette('client/invalid_get') do
-          expect(get_data).not_to be_success
+        VCR.use_cassette('client/not_found_get_from_base_url') do
+          response = get_data
+
+          expect(response).not_to be_success
+          expect(response.status).to eq(404)
         end
       end
     end
 
     context 'when api_key invalid' do
+      let(:api_key) { 'invalid-key' }
+
+      it 'does not succeeds' do
+        VCR.use_cassette('client/unauthorized_get') do
+          response = get_data
+
+          expect(response).not_to be_success
+          expect(response.status).to eq(401)
+        end
+      end
     end
 
     context 'when uri invalid' do
+      let(:url) { 'invalid-resource' }
+
+      it 'does not succeeds' do
+        VCR.use_cassette('client/not_found_get_from_uri') do
+          response = get_data
+
+          expect(response).not_to be_success
+          expect(response.status).to eq(404)
+        end
+      end
     end
 
     context 'when valid request' do
+      it 'succeeds' do
+        VCR.use_cassette('client/success_get') do
+          response = get_data
+
+          expect(response).to be_success
+          expect(response.status).to eq(200)
+          expect(response.content).to be_present
+        end
+      end
     end
   end
 end
